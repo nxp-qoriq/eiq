ecd9f2ea80dd97164517209e0ba72c6fcd37a319
Feng Guo <feng.guo@nxp.com>
2019-08-12 09:41:24 +0000

    Add build script for tflite

---
+++ tflite/build.sh
@@ -0,0 +1,19 @@
+export http_proxy="http://wbi\nxf42681:Welcome%402017@apac.nics.nxp.com:8080"
+export ftp_proxy="http://wbi\nxf42681:Welcome%402017@apac.nics.nxp.com:8080"
+export https_proxy="http://wbi\nxf42681:Welcome%402017@apac.nics.nxp.com:8080"
+
+BUILD_DIR=`pwd`
+echo "Start building in $BUILD_DIR"
+
+apt-get update
+apt-get install -y build-essential
+
+wget https://github.com/tensorflow/tensorflow/archive/v1.12.3.tar.gz
+tar xvf v1.12.3.tar.gz
+cd tensorflow-1.12.3
+cp $BUILD_DIR/0001-Add-build-script-for-aarch64.patch .
+patch -p1 < 0001-Add-build-script-for-aarch64.patch
+./tensorflow/contrib/lite/tools/make/download_dependencies.sh
+./tensorflow/contrib/lite/tools/make/build_aarch64_lib.sh
+cp tensorflow/contrib/lite/tools/make/gen/aarch64_armv8-a/lib/*.a /usr/local/lib
+cp tensorflow/contrib/lite/tools/make/gen/aarch64_armv8-a/bin/* /usr/local/bin
