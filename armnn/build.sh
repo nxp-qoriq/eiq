36cc4cc0cb935e7eca49d0220b74c6f83d64c32d
yanan.yang <yanan.yang@nxp.com>
2019-08-23 09:52:39 +0800

    fix armnn build issue
    Signed-off-by: yanan.yang <yanan.yang@nxp.com>

--- armnn/build.sh
+++ armnn/build.sh
@@ -6,8 +6,7 @@
 #
 # Author: Yanan Yang <yanan.yang@nxp.com>
 #
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=
 DO_PRINT_HELP=false
 INSTALL_DIR=/usr/local
 
@@ -198,13 +197,13 @@ function do_compile_tensorflow_protobuf() {
     if [ ! -d armnn ]; then
         git clone --branch $ARMNN_BRANCH $ARMNN_GIT
         cd armnn
-        git am $BASEDIR/0001-stdlib-issue-work-around.patch
+        git am $SRCDIR/0001-stdlib-issue-work-around.patch
         #git am $SRCDIR/0002-enable-use-of-boost-shared-library.patch
-        git am $BASEDIR/0003-add-more-test-command-line-arguments.patch
-        git am $BASEDIR/0004-generate-versioned-library.patch
-        git am $BASEDIR/0005-add-armnn-mobilenet-test-example.patch
-        git am $BASEDIR/0006-armnn-mobilenet-test-example.patch
-        git am $BASEDIR/0007-enable-use-of-arm-compute-shared-library.patch
+        git am $SRCDIR/0003-add-more-test-command-line-arguments.patch
+        git am $SRCDIR/0004-generate-versioned-library.patch
+        git am $SRCDIR/0005-add-armnn-mobilenet-test-example.patch
+        git am $SRCDIR/0006-armnn-mobilenet-test-example.patch
+        git am $SRCDIR/0007-enable-use-of-arm-compute-shared-library.patch
     fi
     #4. download Tensorflow protobuf library
     cd $BASEDIR/
@@ -273,6 +272,7 @@ function do_build() {
         mkdir -p armnn_build
     fi
 
+    SRCDIR=$(pwd)
     cd armnn_build
     BASEDIR=$(pwd)
 
